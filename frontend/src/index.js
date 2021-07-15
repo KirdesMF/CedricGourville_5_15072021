26b53abc46ff032a045a9f5bc29c713d32e1e790
Cedric Gourville <cedgourville@gmail.com>
2021-07-15 04:13:33 +0200

    ⚗ test front and back

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,17 @@
-console.log('ok');
+document.body.style.background = 'red';
+
+function getImages(teddies) {
+   teddies.forEach((teddy) => {
+      document.body.innerHTML += `<img src="${teddy.imageUrl}" />`;
+   });
+}
+
+function fetchTeddies() {
+   const url = 'http://localhost:3000/api/teddies';
+
+   fetch(url)
+      .then((res) => res.json())
+      .then((data) => getImages(data))
+      .catch((err) => console.log(err));
+}
+fetchTeddies();
