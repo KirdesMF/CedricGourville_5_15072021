e36b12f00f9cedd41a8a641ede252554f5c4008d
Cedric Gourville <cedgourville@gmail.com>
2021-07-18 17:33:20 +0200

    💄 update header home ui

--- frontend/src/js/views/Home.js
+++ frontend/src/js/views/Home.js
@@ -1,24 +1,20 @@
 const LINKS = [
    { path: '/teddies', text: 'Teddies' },
    { path: '/cameras', text: 'Cameras' },
-   { path: '/furniture', text: 'Furniture' },
+   { path: '/furniture', text: 'Furnitures' },
 ];
 
 const anchors = LINKS.map(
-   (link) =>
-      `<a href="${link.path}" class="nav__a" data-router="true">${link.text}</a>`
+   (link) => `<a href="${link.path}" data-router="true">${link.text}</a>`
 ).join('');
 
 export const Home = {
    render: () => {
       return /* html */ `
-         <section class="home panel h100 grid grid-center">
-            <div class="grid-flow  wrapper">
-               <h1>Séléction de nos meilleures ventes</h1>
-               <nav class="nav">
-                  ${anchors}
-               </nav>
-            </div>
+         <section class="home panel h100">
+            <nav class="home__nav wrapper h100">
+               ${anchors}
+            </nav>
          </section>
       `;
    },
