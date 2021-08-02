d3bca34c4f06695b4cd829e448b5de35b3ba502e
Cedric Gourville <cedgourville@gmail.com>
2021-08-02 16:20:04 +0200

    💄 style error 404 page

--- frontend/src/js/views/Error404.js
+++ frontend/src/js/views/Error404.js
@@ -1,9 +1,19 @@
 function render() {
    return /* html */ `
-         <section class="panel">
-            <div class="wrapper">
+         <section class="error panel h100">
+            <div class="wrapper grid-flow grid-items-center  h100">
                <h1>404</h1>
-               <a href="/" data-router="true">Error you should go back</a>
+               <a class="back" href="/" data-router="true">
+                  <svg
+                     class="svg-icon"
+                     focusable="false"
+                     role="img"
+                     aria-hidden="true"
+                  >
+                     <use href="#chevron-icon" />
+                  </svg>
+                  <span>There is something wrong</span>
+               </a>
             </div>
          </section>
       `;
